9a7a61a3973937dd9e65528e0125880df6dfb96f
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-03-10 08:36:40 -0300

    update

---
+++ 1 - classes em java/pacote2/Classe3.java
@@ -0,0 +1,11 @@
+package pacote2;
+
+import pacote1.Classe2;
+
+public class Classe3 extends Classe2 {
+  
+  public static void main(String[] args) {
+    Classe2 classe2 = new Classe2();
+    
+  }
+}
