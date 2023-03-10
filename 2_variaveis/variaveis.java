aef3955afd7cd063b49ed9e22b0aba5334c199ee
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-03-10 10:22:00 -0300

    upload variaveis

---
+++ 2_variaveis/variaveis.java
@@ -0,0 +1,24 @@
+public class variaveis {
+  
+  private int codigo;
+  private long codigoMaior;
+  private double valorDecimal2;
+  private String texto;
+  private boolean status;
+
+  private float valorDecimal;
+  private short codigoPequeno;
+  private byte codigoMenorAinda;
+
+  public String retornaTexto() {
+    return "opa";
+  }
+
+  public int retornaInteiro() {
+    return 1999999999;
+  }
+
+  public long retornaLong(long num) {
+    return num;
+  }
+}
