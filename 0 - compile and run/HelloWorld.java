3f4733c83557a7bfb6eaca84d5c6d98845b911f8
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-03-10 07:15:39 -0300

    upload

---
+++ 0 - compile and run/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  
+  public static void main(String[] args) {
+    String ambiente = args[0];
+    if (ambiente.equals("DEV")) {
+      System.out.println("Executando em DEV");
+    } else if (ambiente.equals("TEST")) {
+      System.out.println("Executando de TEST");
+    } else {
+      System.out.println(ambiente);
+    }
+  }
+}
