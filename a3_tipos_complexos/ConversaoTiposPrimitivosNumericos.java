48c8e66afb5b2e43a0a207e47eec464d882165c1
Emanoel Henrick <emanoelhenrick.dev@gmail.com>
2023-03-13 10:56:18 -0300

    update tipos complexos

---
+++ a3_tipos_complexos/ConversaoTiposPrimitivosNumericos.java
@@ -0,0 +1,21 @@
+package a3_tipos_complexos;
+
+public class ConversaoTiposPrimitivosNumericos {
+  public static void main(String[] args) {
+    
+    double a = 1;
+    System.out.println(a);
+
+    float b = (float) 1.0;
+    System.out.println(b);
+
+    int c = 130;
+    byte d = (byte) c;
+    System.out.println(d);
+
+    double e = 1.999999;
+    int f = (int) e;
+    System.out.println(f);
+
+  }
+}
